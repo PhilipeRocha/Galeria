5a946bf97baa6fb7b14a367d109b3ebe695fd9b0
Philipe Rocha <68014636+PhilipeRocha@users.noreply.github.com>
2020-11-17 17:57:02 -0300

    enviando o projeto galeria para o Github

---
+++ project/src/index.js
@@ -0,0 +1,12 @@
+//Arquivo SASS
+import "./scss/index.scss"
+
+//Dependencias
+
+import "jquery"
+import "bootstrap"
+
+//Arquivos JS
+
+import "./js/core/includes"
+import "./js/plugins/cityButtons"
